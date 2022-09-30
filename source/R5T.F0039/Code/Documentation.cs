f29b3f83d3b02eb1ddeefa9fd3bf2505c73427f7
davidcoats <david@getsafetycone.com>
2022-09-30 16:36:43 -0700

    Initial commit.

---
+++ source/R5T.F0039/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0039
+{
+	/// <summary>
+	/// Quartz scheduler functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0039/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.F0039
+{
+    public static class Instances
+    {
+		
+    }
+}
